600757eeacf4e565d0f82d71ba28b64defe48c8f
mobamike <mike@mobanode.com>
2016-09-08 12:27:56 +0100

    Master now uses production API Endpoint. Fixed latest version number in docs

--- sdk/src/main/java/com/popdeem/sdk/core/api/PDAPIConfig.java
+++ sdk/src/main/java/com/popdeem/sdk/core/api/PDAPIConfig.java
@@ -35,7 +35,7 @@ public class PDAPIConfig {
     public static final String PD_PROD_API_ENDPOINT = "http://api.popdeem.com";         // Production API Endpoint
     public static final String PD_STAGING_API_ENDPOINT = "https://staging.popdeem.com";  // Staging API Endpoint
 
-    public static final String PD_API_ENDPOINT = PD_STAGING_API_ENDPOINT;
+    public static final String PD_API_ENDPOINT = PD_PROD_API_ENDPOINT;
 
     private static final String API_PATH = "/api/";
     public static final String API_VERSION = "v2";
